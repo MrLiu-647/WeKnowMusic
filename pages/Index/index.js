b1fc769f8ad9006af31da0b3305962598955fd89
liusiqi <510137672@qq.com>
2019-08-20 18:10:44 +0800

    解决登录不判断bug

--- pages/Index/index.js
+++ pages/Index/index.js
@@ -13,9 +13,7 @@ Page({
     // 查看是否授权
     wx.getSetting({
       success: function (res) {
-        wx.switchTab({
-          url: '/pages/WTHome/WTHome',
-        })
+        
         if (res.authSetting['scope.userInfo']) {
           wx.getUserInfo({
             lang: "zh_CN",
@@ -23,6 +21,9 @@ Page({
               // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
               // 根据自己的需求有其他操作再补充
               // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
+              wx.switchTab({
+                url: '/pages/WTHome/WTHome',
+              })
               wx.login({
                 success: res => {
                   console.log(res)
